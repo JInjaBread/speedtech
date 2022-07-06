2e4d43db9b1a3c4c17a223b211ea0e64bc086eec
jinja <jovanbeato3322@gmail.com>
2022-07-06 18:44:01 +0800

    Version 1.2.1

--- speedtech/settings.py
+++ speedtech/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure--=@zy^t4n)=t4_owsz$^l_75zt@90yr2^o1z5u57bg1sl_tvt^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['speedtechmotor.herokuapp.com']
 
 
 # Application definition
